180a427920c4f80ac41a5ae9e39df7c93222b89e
Finn Meijeringh <finn.meijeringh864@gmail.com>
2024-06-02 14:14:34 +0200

    add car movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public float turningSpeed;
-    public float horizontalInput;
-    public float verticalInput;
+    private float turningSpeed = 120;
+    private float horizontalInput;
+    private float verticalInput;
+    private bool accelerate;
+    private bool decelerate;
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
@@ -20,7 +23,49 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
-        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * speed);
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            accelerate = true;
+        } else if (Input.GetKeyUp(KeyCode.W))
+        {
+            accelerate = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            decelerate = true;
+        } else if (Input.GetKeyUp(KeyCode.S))
+        {
+            decelerate = false;
+        }
+
+        if (accelerate)
+        {
+            int variable = 2;
+            if (speed <= 80) speed += variable;
+        }
+        else if (!accelerate && speed != 0)
+        {
+            if (speed > 0) speed -= 1.0f;
+        }
+
+        if (decelerate)
+        {
+            int variable = 1;
+            if (speed >= -20) speed -= variable;
+        }
+        else if (!decelerate && speed != 0)
+        {
+            if (speed < 0) speed += 1;
+        }
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        float invertedHorizontalInput = horizontalInput * Mathf.Sign(verticalInput);
+
+        if (speed > 0) transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turningSpeed);
+        if (speed < 0) transform.Rotate(Vector3.up, invertedHorizontalInput * Time.deltaTime * turningSpeed);
     }
 }
